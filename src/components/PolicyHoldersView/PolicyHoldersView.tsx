2176ca6a09d499f14128c74f519863c181568d43
Dan G Nelson <dangnelson@users.noreply.github.com>
2023-01-13 15:38:21 -0500

    Challenge 6

--- src/components/PolicyHoldersView/PolicyHoldersView.tsx
+++ src/components/PolicyHoldersView/PolicyHoldersView.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import InfoTable from '../InfoTable';
 
 function PolicyholdersView() {
-  const [policyHolders, setPolicyHolders] = useState<any>([]);
+  const [policyHolder, setPolicyHolder] = useState<
+    Array<{
+      key: string;
+      value: string | number;
+    }>
+  >([]);
 
   useEffect(() => {
     const sessionData = sessionStorage.getItem('policyHolders');
 
     if (sessionData) {
-      setPolicyHolders(JSON.parse(sessionData));
+      setPolicyHolder(JSON.parse(sessionData));
     } else {
       const fetchData = async () => {
         try {
@@ -16,8 +22,45 @@ function PolicyholdersView() {
             'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders'
           );
           const json = await response.json();
-          setPolicyHolders(json);
-          sessionStorage.setItem('policyHolders', JSON.stringify(json));
+          const firstResponse = json.policyHolders[0];
+          const newPolicyHolder = [
+            {
+              key: 'Name',
+              value: firstResponse.name,
+            },
+            {
+              key: 'Age',
+              value: firstResponse.age,
+            },
+            {
+              key: 'Address',
+              value:
+                firstResponse.address.line1 +
+                ' ' +
+                firstResponse.address.line2 +
+                ', ' +
+                firstResponse.address.city +
+                ', ' +
+                firstResponse.address.state +
+                ' ' +
+                firstResponse.address.postalCode,
+            },
+            {
+              key: 'Phone Number',
+              value: firstResponse.phoneNumber,
+            },
+            {
+              key: 'Primary Policyholder?',
+              value: firstResponse.isPrimary ? 'Yes' : 'No',
+            },
+          ];
+
+          setPolicyHolder(newPolicyHolder);
+
+          sessionStorage.setItem(
+            'policyHolders',
+            JSON.stringify(newPolicyHolder)
+          );
         } catch (error) {
           console.log('error', error);
         }
@@ -31,7 +74,11 @@ function PolicyholdersView() {
     };
   }, []);
 
-  return <Box></Box>;
+  return (
+    <Box>
+      <InfoTable header="Policy Holder" rows={policyHolder} />
+    </Box>
+  );
 }
 
 export default PolicyholdersView;
